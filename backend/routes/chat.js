aead444bd3aecf59b8ab4d6b10950339e6774fcc
Bhuvan Bhonde <bhuvanbhonde712@gmail.com>
2026-03-03 15:16:11 +0530

    Fix import case for Chat model in chat.js

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Chat = require('../models/Chat')
+const Chat = require('../models/chat')
 
 // GET chats
 router.get('/', async (req, res) => {
