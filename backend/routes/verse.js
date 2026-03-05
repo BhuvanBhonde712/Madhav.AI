4a3bd83b869a1acb65dc8e5fa35fb98603cfaec7
Bhuvan Bhonde <bhuvanbhonde712@gmail.com>
2026-03-05 14:14:14 +0530

    Update verse.js

--- backend/routes/verse.js
+++ backend/routes/verse.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { explainVerse } = require('../controllers/verseController');
+const { explainVerse } = require('../controllers/versecontroller');
 const { protect } = require('../middleware/auth');
 
 router.post('/explain', protect, explainVerse);
+
 
 module.exports = router;
