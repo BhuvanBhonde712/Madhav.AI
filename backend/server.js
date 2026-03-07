4daa5c2b4cafa61379554159a371d94fc0312ee8
Bhuvan Bhonde <bhuvanbhonde712@gmail.com>
2026-03-07 16:05:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,4 +49,3 @@ app.use((req, res) => res.status(404).json({ error: 'Route not found' }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
